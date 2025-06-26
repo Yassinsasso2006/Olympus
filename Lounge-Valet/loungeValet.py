2b1c4e389bff6c6e83e4569a2993febaae3acb41
Yassinsasso <yassinyamen121@gmail.com>
2025-06-26 05:05:34 +0300

    Updating structure and stating work on the verfication bot

---
+++ Lounge-Valet/loungeValet.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads .env file
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True  # needed for reading messages
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot is online as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
