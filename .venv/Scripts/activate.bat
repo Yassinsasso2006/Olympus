e3db3d7e6625ba473398f1b898ab62fcb6f0fa73
Yassinsasso <yassinyamen121@gmail.com>
2025-07-10 21:21:17 +0300

    Downloading discord prequiste again

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Github\Writer's Lounge\Master-Bot-System\.venv
+set VIRTUAL_ENV=e:\Github\Writer's Lounge\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
